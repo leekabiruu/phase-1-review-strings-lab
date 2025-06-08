961e63fffdeba71c1bd418addda8076c32812fb0
Lee <leekinyuakabiru@gmail.com>
2025-06-08 10:32:00 +0300

    Review Strings Lab

--- index.js
+++ index.js
@@ -1,1 +1,4 @@
-// Write your code in this file!
+let currentUser=`LEE`;
+let welcomeMessage=`Welcome to Flatbook, ${currentUser}!`;
+let excitedWelcomeMessage=`WELCOME TO FLATBOOK, LEE!`;
+let shortGreeting="Welcome, L !"
